32bfaec17ec3a9212fc269423c49006966323cce
kesava-wow <kesava-wow@users.noreply.github.com>
2015-06-17 02:32:00 +0100

    relocate ouf embed

---
+++ lib/KuiConfig.lua
@@ -0,0 +1,14 @@
+--[[
+-- Simple configuration library with profiles.
+-- By Kesava @ curse.com.
+-- All rights reserved.
+--]]
+local MAJOR, MINOR = 'KuiConfig-1.0', 1
+local kc = LibStub:NewLibrary(MAJOR, MINOR)
+
+if not kc then
+	-- already registered
+	return
+end
+
+
